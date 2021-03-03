245fddf6808253a78f00d6456a10682197514dec
Marco <m.j.deken@student.vu.nl>
2021-03-03 18:12:03 +0100

    Improving q_learning

--- attempt2.py
+++ attempt2.py
@@ -24,23 +24,19 @@ nr_feats = 2
 def main(options):
     env = flappy_bird_gym.make("FlappyBird-v0")
 
-    # for i in range(10):
-    #     info = play_game(env, options.verbose, options.show_gui, options.fps)
-
     repeat = 0
     overwrite = 1
-    filename = "q_table4.npy"
+    filename = "q_table.npy"
     with open(filename, 'rb') as f:
         q_table = np.load(f)
 
     if overwrite or not os.path.isfile(filename):
-        start = datetime.now()
-        q_table = np.zeros([nr_states_h, nr_states_v, env.action_space.n])
-        # q_table, all_scores, all_rewards, observation_history = q_learning(env, q_table)
+        start1 = datetime.now()
+        # q_table = np.zeros([nr_states_h, nr_states_v, env.action_space.n])
         q_table, all_scores, all_rewards = q_learning(env, q_table)
         np.save(filename, q_table)
-        end = datetime.now()
-        print("\nTraining took %.2f mins\n"%((end-start).seconds/60))
+        end1 = datetime.now()
+        print("\nTraining took %.2f mins\n"%((end1-start1).seconds/60))
 
     if overwrite:
         dec = 10
@@ -52,71 +48,51 @@ def main(options):
         q_table = np.load(f)
 
     # play_q_game(q_table, env, fps=20)
-    # print(q_table.shape)
-    print(len(q_table[np.all(q_table==0.0,axis=2)]))
+    print("Proportion of q_table that is empty: %.2f%%" %(len(q_table[np.all(q_table==0.0,axis=2)])/nr_states_h/nr_states_v*100))
 
-    # h_zeros = np.zeros(nr_states_h)
-    # v_zeros = np.zeros(nr_states_v)
-    # for i in range(nr_states_h):
-    #     for j in range(nr_states_v):
-    #         if any(q_table[i,j] == 0.0):
-    #             h_zeros[i] += 1
-    #             v_zeros[j] += 1
-
-    # plt.plot(h_zeros)
-    # plt.title("h_dist")
-    # plt.figure()
-    # plt.plot(v_zeros)
-    # plt.title("v_dist")
-
-    repeat = 10000
-    start = datetime.now()
+    repeat = 100000
+    start2 = datetime.now()
     results = np.zeros(repeat)
     for i in range(repeat):
         # results[i] = MarcoCarlo.play_game(env, show_prints=False, show_gui=False)['score']
         results[i] = play_q_game(q_table, env, show_prints=False, show_gui=False)['score']
+    end2 = datetime.now()
 
-    end = datetime.now()
-
-    # plt.subplot(1,3,1)
-    # plt.title("h_dist")
-    # plt.hist(observation_history[:,0], bins=40)
-    # plt.subplot(1,3,2)
-    # plt.title("v_dist")
-    # plt.hist(observation_history[:,1], bins=40)
-    # plt.subplots_adjust(wspace=0.3)
 
     if overwrite:
         fig, axs = plt.subplots(2,1)
         plt.subplots_adjust(hspace=0.8)
-        axs[0].plot(selected_scores, 'r', linewidth=0.7)
+
+        axs[0].plot(all_scores, 'ob', alpha=0.1, markersize=2)
+        axs[0].plot(np.arange(len(all_scores))[all_scores>3], all_scores[all_scores>3], 'ob', alpha=1, markersize=2)
         axs[0].set_title("Learning progress")
         axs[0].set_xlabel("Iteration")
         axs[0].set_ylabel("Points scored")
-        # axs[0].set_xlim([0,repeat])
+        axs[0].set_xlim([0,len(all_scores)])
 
-        axs[1].plot(all_rewards, 'r', linewidth=0.7)
-        axs[1].set_title("Rewards")
-        axs[1].set_xlabel("Iteration")
-        axs[1].set_ylabel("Rewards earned")
-        # axs[1].set_xlim([0,repeat])
+        axs[1].hist(all_scores, bins=50, color='red')
+        axs[1].set_ylabel("Frequency")
+        axs[1].set_xlabel("Points scored")
+        axs[1].set_title("Took %.2f mins"%((end1-start1).seconds/60))
+        axs[1].set_xlim(0)
+        plt.savefig("q_learning_results.png")
 
     if repeat:
         fig, axs = plt.subplots(2,1)
         plt.subplots_adjust(hspace=0.8)
 
         axs[0].plot(results, 'r', linewidth=0.7)
-        axs[0].set_title("Learning progress")
+        axs[0].set_title("After learning")
         axs[0].set_xlabel("Iteration")
         axs[0].set_ylabel("Points scored")
 
         axs[1].hist(results, bins=50, color='red')
         axs[1].set_ylabel("Frequency")
         axs[1].set_xlabel("Points scored")
-        axs[1].set_title("Took %.2f mins"%((end-start).seconds/60))
+        axs[1].set_title("Took %.2f mins"%((end2-start2).seconds/60))
         axs[1].set_xlim(0)
+        plt.savefig("q_playing_results.png")
 
-    plt.savefig("q_learning_results.png")
 
 
 def q_learning(env, q_table):
@@ -134,7 +110,6 @@ def q_learning(env, q_table):
     # For plotting metrics
     all_scores = np.zeros(repeat)
     all_rewards = np.zeros(repeat)
-    # observation_history = np.zeros((1,nr_feats))
     # states_visited = [[], []]
 
     for i in range(repeat):
@@ -156,8 +131,6 @@ def q_learning(env, q_table):
             if random.uniform(0, 1) < epsilon:
                 action = env.action_space.sample() # Explore action space
             else:
-                # print(q_table[state])
-                # print(np.argmax(q_table[state]))
                 action = np.argmax(q_table[state]) # Exploit learned values
 
 
@@ -170,7 +143,6 @@ def q_learning(env, q_table):
             next_state = discrete_state(next_obs)
             # states_visited[0].append(next_state[0])
             # states_visited[1].append(next_state[1])
-            # observation_history = np.concatenate((observation_history, next_obs.reshape(1,-1)))
 
             old_value = q_table[state][action]
             next_max = np.max(q_table[next_state])
@@ -189,7 +161,7 @@ def q_learning(env, q_table):
 
     # print("\n",len(states_visited[0]),len(states_visited[1]))
 
-    return q_table, all_scores, all_rewards#, observation_history[1:]
+    return q_table, all_scores, all_rewards
 
 
 def discrete_state(obs):
@@ -212,8 +184,6 @@ def v_state(v_dist):
     # states 0-99
     min_value = -0.10
     max_value = 0.10
-    # min_value = -0.50
-    # max_value = 0.50
 
     if v_dist < -0.3:
         return 0
@@ -231,13 +201,10 @@ def v_state(v_dist):
         return nr_states_v-3
     elif v_dist > max_value:
         return nr_states_v-4
-    if v_dist < min_value:
-        return 0
-    elif v_dist > max_value:
-        return nr_states_v-1
 
     #first scale between 0 and 1 then distribute over the remaining nr of states
     return int((v_dist-min_value)/(max_value-min_value) * (nr_states_v-8)) + 1
+
 
 def play_q_game(q_table, env=0, show_prints=True, show_gui=True, fps=100):
 
@@ -263,10 +230,10 @@ def play_q_game(q_table, env=0, show_prints=True, show_gui=True, fps=100):
 
         if show_prints:
             # if prev_score != info['score']:
-            now = datetime.now().second
-            if prev_sec != now:
+            # now = datetime.now().second
+            # if prev_sec != now:
             # if reward > 0:
-                prev_sec = now
+                # prev_sec = now
                 # prev_score = info['score']
 
                 print(obs)
@@ -336,8 +303,6 @@ def play_game(env=0, show_prints=False, show_gui=False, fps=100):
     env.close()
 
     return info
-
-
 
 
 
